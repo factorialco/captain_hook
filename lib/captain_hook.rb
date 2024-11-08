5fb19309c1dbd2557f954f0314ee2f5ea5a36af1
Antonio Tapiador del Dujo <antonio.tapiador@factorial.co>
2024-11-08 16:17:53 +0000

    wip

--- lib/captain_hook.rb
+++ lib/captain_hook.rb
@@ -80,6 +80,7 @@ module CaptainHook
 
     def method_excluded_by_all_hooks?(method)
       get_hooks(:before).all? { |hook| hook.exclude.include?(method) } &&
+        get_hooks(:around).all? { |hook| hook.exclude.include?(method) } &&
         get_hooks(:after).all? { |hook| hook.exclude.include?(method) }
     end
 
